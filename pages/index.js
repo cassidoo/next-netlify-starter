cd5852388cfdea9725a1d588e14aa7218c17b2cb
alexchoiweb <alexchoiweb@gmail.com>
2021-01-13 12:10:39 -0800

    add print button

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  const print = () => {
+    window.print();
+  }
   return (
     <div className="container">
+      <button onClick={print}>Print Page</button>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
